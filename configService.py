20d186a1daef9e2eeb49e831cf5c67e2490b852e
Peradat Permjai <izemkung@GMAIL.COM>
2021-03-17 02:28:19 +0700

    fixError

--- configService.py
+++ configService.py
@@ -278,18 +278,4 @@ while(True):
         lastTimeTask3 = currentTime + 60
         sendStatusPack( 'online', (currentTime - timeStart)/60 )
 
-    if(GPIO.input(4) == 0):
-        sendStatusPack( 'Power Off', (currentTime - timeStart)/60 )
-        print("Pi Power Off Process!!")
-        time.sleep(1)
-        i = 0
-        while( i < 20):
-            i += 1
-            time.sleep(0.2)
-            GPIO.output(17,True)
-            time.sleep(0.2)
-            GPIO.output(17,False)
-        os.system('sudo shutdown -h now')
-        break
-
-    time.sleep(1)
+    
--- readGPIO.py
+++ readGPIO.py
@@ -20,10 +20,25 @@ USBDEVFS_RESET= 21780
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(27, GPIO.IN)#3G
+GPIO.setup(4, GPIO.IN) # Power
 print("LED 3G GPIO(27)")
 print(GPIO.input(27))
 
 while True:
-    time.sleep(600)
+    if(GPIO.input(4) == 0):
+        sendStatusPack( 'Power Off', (currentTime - timeStart)/60 )
+        print("Pi Power Off Process!!")
+        time.sleep(1)
+        i = 0
+        while( i < 20):
+            i += 1
+            time.sleep(0.2)
+            GPIO.output(17,True)
+            time.sleep(0.2)
+            GPIO.output(17,False)
+        os.system('sudo shutdown -h now')
+        break
+
+    time.sleep(1)
 
        
